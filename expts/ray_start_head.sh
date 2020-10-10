81157970fd8b7b35f2999f9a68aee3e88dc9c521
Kiran Vaddi <kiranvad@cpn-p28-07.cbls.ccr.buffalo.edu>
2020-10-09 20:37:43 -0400

    cleaning up master for windows installation

--- expts/ray_start_head.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "starting ray head node"
-# Launch the head node
-ray start --head --webui-host 0.0.0.0 --node-ip-address=$1 --port=6379 --redis-password=$2
-sleep infinity
--- expts/ray_start_worker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "starting ray worker node"
-ray start --address $1 --redis-password=$2
-sleep infinity
