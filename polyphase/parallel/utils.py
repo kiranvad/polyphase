6dd31f5f65655b7a181882371c4e8850b17e3ff2
Balaji Pokuri <balajip@iastate.edu>
2020-10-19 20:23:12 -0500

    [minor] reformatting for PEP8

--- polyphase/parallel/utils.py
+++ polyphase/parallel/utils.py
@@ -5,18 +5,20 @@ from ..helpers import timer
 
 from scipy.spatial.distance import squareform
 
+
 @ray.remote
 def _get_distance_row(data, metric, rowid, triuids):
     print('Staging {} on {}'.format(rowid, ray.services.get_node_ip_address()))
     T = timer()
     rowid_flags = triuids[0]
-    rows = triuids[0][rowid_flags==rowid]
-    cols = triuids[1][rowid_flags==rowid]
+    rows = triuids[0][rowid_flags == rowid]
+    cols = triuids[1][rowid_flags == rowid]
     dist_row = []
-    for r,c in zip(rows, cols):
+    for r, c in zip(rows, cols):
         dist_row.append(metric(data[r], data[c]))
 
     return dist_row, rowid, T.end()
+
 
 def get_distance_matrix(X, metric):
     """ Compute distance matrix in parallel using ray
@@ -53,36 +55,37 @@ def get_distance_matrix(X, metric):
     ray.init(ignore_reinit_error=True)
     T = timer()
     n_samples = len(X)
-        
-    nC2 = 0.5*(n_samples*(n_samples-1))
-    iu = np.triu_indices(n_samples,1)
+
+    nC2 = 0.5 * (n_samples * (n_samples - 1))
+    iu = np.triu_indices(n_samples, 1)
     row_ids = np.unique(iu[0])
-    
+
     iu_ray = ray.put(iu)
     X_ray = ray.put(X)
     metric_ray = ray.put(metric)
-    
+
     remaining_result_ids = [_get_distance_row.remote(X_ray, metric_ray, rowid, iu_ray) for rowid in row_ids]
-    
+
     dist = {}
     while len(remaining_result_ids) > 0:
         ready_result_ids, remaining_result_ids = ray.wait(remaining_result_ids, num_returns=1)
         result_id = ready_result_ids[0]
-        dist_result,rowid_result, time_result = ray.get(result_id)
+        dist_result, rowid_result, time_result = ray.get(result_id)
         dist[rowid_result] = dist_result
-        print('Processed : {} took {} '.format(rowid_result, time_result)) 
-        
-    del remaining_result_ids,result_id, iu_ray, metric_ray, X_ray, dist_result,rowid_result, time_result      
-    
+        print('Processed : {} took {} '.format(rowid_result, time_result))
+
+    del remaining_result_ids, result_id, iu_ray, metric_ray, X_ray, dist_result, rowid_result, time_result
+
     reduce_dist = [dist[k] for k in sorted(dist)]
     dist = np.hstack(reduce_dist)
 
-    assert nC2==len(dist), "Not all the reduced distances are returned. expected {} got {}".format(nC2, len(dist))
-    
+    assert nC2 == len(dist), "Not all the reduced distances are returned. expected {} got {}".format(nC2, len(dist))
+
     D = squareform(dist)
-    
-    assert n_samples==np.shape(D)[0] , "Shape of distance matrix is {}, expected {}x{}".format(D.shape, n_samples, n_samples)
-    
+
+    assert n_samples == np.shape(D)[0], "Shape of distance matrix is {}, expected {}x{}".format(D.shape, n_samples,
+                                                                                                n_samples)
+
     print('\nComputation took : {}'.format(T.end()))
 
     return D
--- polyphase/utils.py
+++ polyphase/utils.py
@@ -11,19 +11,20 @@ import plotly.graph_objects as go
 import plotly.figure_factory as ff
 from scipy.spatial import Delaunay
 
-from .helpers import get_ternary_coords 
+from .helpers import get_ternary_coords
 from .parphase import _utri2mat
 from scipy.constants import gas_constant
 
+
 def set_ternlabel(ax):
-    ax.set_tlabel("$\\varphi_{p1}$",fontsize=15)
-    ax.set_llabel("$\\varphi_{s}$",fontsize=15)
-    ax.set_rlabel("$\\varphi_{p2}$",fontsize=15)
+    ax.set_tlabel("$\\varphi_{p1}$", fontsize=15)
+    ax.set_llabel("$\\varphi_{s}$", fontsize=15)
+    ax.set_rlabel("$\\varphi_{p2}$", fontsize=15)
     ax.taxis.set_label_position('corner')
     ax.laxis.set_label_position('corner')
     ax.raxis.set_label_position('corner')
     sns.axes_style("ticks")
-    
+
     return ax
 
 
@@ -33,74 +34,78 @@ def plot_energy_landscape(outdict):
     tri_coords = np.array([get_ternary_coords(pt) for pt in grid.T])
     energy = outdict['energy']
     simplices = outdict['simplices']
-    #simplices = outdict['hull'].simplices
-    
-    fig = plt.figure(figsize=(4*1.6, 4))
+    # simplices = outdict['hull'].simplices
+
+    fig = plt.figure(figsize=(4 * 1.6, 4))
     ax = fig.add_subplot(1, 1, 1, projection='3d')
-    ax.plot_trisurf(grid[0,:], grid[1,:], energy, triangles=simplices, linewidth=0.2,  antialiased=True, color=(0,0,0,0), edgecolor='Gray')
+    ax.plot_trisurf(grid[0, :], grid[1, :], energy, triangles=simplices, linewidth=0.2, antialiased=True,
+                    color=(0, 0, 0, 0), edgecolor='Gray')
     ax.set_xlabel(r'$\phi_1$')
     ax.set_ylabel(r'$\phi_2$')
     ax.set_zlabel('Energy')
-    
+
     plt.show()
 
 
-def plot_triangulated_surface(u, v, x,y,z, **kwargs):
-    points2D = np.vstack([u,v]).T
+def plot_triangulated_surface(u, v, x, y, z, **kwargs):
+    points2D = np.vstack([u, v]).T
     tri = Delaunay(points2D)
     simplices = tri.simplices
     fig = ff.create_trisurf(x=x, y=y, z=z,
-                         simplices=simplices, **kwargs)
-    
+                            simplices=simplices, **kwargs)
+
     return fig
 
+
 def make_torus():
-    u = np.linspace(0, 2*np.pi, 20)
-    v = np.linspace(0, 2*np.pi, 20)
-    u,v = np.meshgrid(u,v)
+    u = np.linspace(0, 2 * np.pi, 20)
+    v = np.linspace(0, 2 * np.pi, 20)
+    u, v = np.meshgrid(u, v)
     u = u.flatten()
     v = v.flatten()
 
-    x = (3 + (np.cos(v)))*np.cos(u)
-    y = (3 + (np.cos(v)))*np.sin(u)
+    x = (3 + (np.cos(v))) * np.cos(u)
+    y = (3 + (np.cos(v))) * np.sin(u)
     z = np.sin(v)
-    
+
     return u, v, x, y, z
+
 
 def make_mobious_strip():
-    u = np.linspace(0, 2*np.pi, 24)
+    u = np.linspace(0, 2 * np.pi, 24)
     v = np.linspace(-1, 1, 8)
-    u,v = np.meshgrid(u,v)
+    u, v = np.meshgrid(u, v)
     u = u.flatten()
     v = v.flatten()
 
-    tp = 1 + 0.5*v*np.cos(u/2.)
-    x = tp*np.cos(u)
-    y = tp*np.sin(u)
-    z = 0.5*v*np.sin(u/2.)
-    
+    tp = 1 + 0.5 * v * np.cos(u / 2.)
+    x = tp * np.cos(u)
+    y = tp * np.sin(u)
+    z = 0.5 * v * np.sin(u / 2.)
+
     return u, v, x, y, z
+
 
 def test_plot_triangulated_surface():
     u, v, x, y, z = make_mobious_strip()
     fig = plot_triangulated_surface(u, v, x, y, z)
-    fig.update_layout(title=config_str,scene=dict(
+    fig.update_layout(title=config_str, scene=dict(
         xaxis_title="x",
         yaxis_title="y",
-        zaxis_title = "z"),
-        coloraxis_colorbar=dict(title='z'),
-        font=dict(
-            family="Courier New, monospace",
-            size=18,
-            color="RebeccaPurple")
-    )
-    fig.write_html('../figures/3dplots/test_mobious.html')    
-    
+        zaxis_title="z"),
+                      coloraxis_colorbar=dict(title='z'),
+                      font=dict(
+                          family="Courier New, monospace",
+                          size=18,
+                          color="RebeccaPurple")
+                      )
+    fig.write_html('../figures/3dplots/test_mobious.html')
 
-    
+
 """ Compute chi from solubilities """
 
-def _compute_chi(delta_i,delta_j,V):
+
+def _compute_chi(delta_i, delta_j, V):
     """
     total solubility parameters delta_i, delta_j are computed from hydrogen, polar, dispersive components
     
@@ -109,42 +114,45 @@ def _compute_chi(delta_i,delta_j,V):
     returns a scalar chi value
     
     """
-    constant = 1.0 #4.184*(2.045**2)/(8.314)
-    chi_ij =  0.34+(constant)*(V/(gas_constant*300)*( np.asarray(delta_i) - np.asarray(delta_j) )**2)
-        
+    constant = 1.0  # 4.184*(2.045**2)/(8.314)
+    chi_ij = 0.34 + (constant) * (V / (gas_constant * 300) * (np.asarray(delta_i) - np.asarray(delta_j)) ** 2)
+
     return chi_ij
 
-def _compute_weighted_chi(vec1,vec2,V, W):
+
+def _compute_weighted_chi(vec1, vec2, V, W):
     value = 0.0
-    for i,w  in enumerate(W):
-        value += w*(vec1[i]-vec2[i])**2
-    
-    value = 0.34 + value*(V/(gas_constant*300))
-    
+    for i, w in enumerate(W):
+        value += w * (vec1[i] - vec2[i]) ** 2
+
+    value = 0.34 + value * (V / (gas_constant * 300))
+
     return value
-                   
+
+
 def get_chi_vector(deltas, V0, approach=1):
     """
     Given a list of deltas, computes binary interactions of chis
     """
-    combs = combinations(deltas,2)
-    inds = list((i,j) for ((i,_),(j,_)) in combinations(enumerate(deltas), 2))
-      
-    if approach==1:
-        chi = [_compute_chi(np.linalg.norm(i[0]),np.linalg.norm(i[1]),V0) for i in combs]
-    elif approach==2:
-        chi = [_compute_weighted_chi(i[0],i[1],V0, W = [1.0,1.0,1.0]) for i in combs] 
-    elif approach==3:
-        chi = [_compute_weighted_chi(i[0],i[1],V0, W = [1.0,0.25,0.25]) for i in combs]            
+    combs = combinations(deltas, 2)
+    inds = list((i, j) for ((i, _), (j, _)) in combinations(enumerate(deltas), 2))
+
+    if approach == 1:
+        chi = [_compute_chi(np.linalg.norm(i[0]), np.linalg.norm(i[1]), V0) for i in combs]
+    elif approach == 2:
+        chi = [_compute_weighted_chi(i[0], i[1], V0, W=[1.0, 1.0, 1.0]) for i in combs]
+    elif approach == 3:
+        chi = [_compute_weighted_chi(i[0], i[1], V0, W=[1.0, 0.25, 0.25]) for i in combs]
     else:
         raise KeyError
-        
+
     return chi, inds
-    
+
 
 """ utilities for some example runs """
 
-def get_data(name='ow',fhid=0):
+
+def get_data(name='ow', fhid=0):
     """
     For a given data name and produces a M and Chi parameters of a ternary system.
     If the name is  FHPaper, it requires an id in fhid (default 0 ) in the range of (0,4)
@@ -158,47 +166,48 @@ def get_data(name='ow',fhid=0):
     https://dspace.mit.edu/bitstream/handle/1721.1/35312/76904537-MIT.pdf?sequence=2&isAllowed=y
     
     """
-    floryhuggins =[{'name': 'CHCl3/F8/PCBM','chi': [0.341,0.885,0.941]},
-                  {'name': 'chlorobenzene/PFB/PCBM','chi': [0.340,0.899,0.899]},
-                  {'name': 'CHCl3/APFO-3/PCBM','chi': [0.505,0.885,0.450]},
-                  {'name': 'chlorobenzene/APFO-3/PCBM','chi': [0.480,0.899,0.479]}, 
-                  {'name': 'xylene/PFB/PCBM','chi': [0.442,1.630,1.013]}
-                   ]
-    N = {'CHCl3':1,'chlorobenzene':1,'xylene':1,'F8':720,'PFB':73,'F8BT':172,'APFO-3':66,'PCBM':5}
+    floryhuggins = [{'name': 'CHCl3/F8/PCBM', 'chi': [0.341, 0.885, 0.941]},
+                    {'name': 'chlorobenzene/PFB/PCBM', 'chi': [0.340, 0.899, 0.899]},
+                    {'name': 'CHCl3/APFO-3/PCBM', 'chi': [0.505, 0.885, 0.450]},
+                    {'name': 'chlorobenzene/APFO-3/PCBM', 'chi': [0.480, 0.899, 0.479]},
+                    {'name': 'xylene/PFB/PCBM', 'chi': [0.442, 1.630, 1.013]}
+                    ]
+    N = {'CHCl3': 1, 'chlorobenzene': 1, 'xylene': 1, 'F8': 720, 'PFB': 73, 'F8BT': 172, 'APFO-3': 66, 'PCBM': 5}
     fname = name
     if name is 'ow':
-        M = np.array([5,1,5])
-        chi = [0.5,1,0.5] # chi_12, chi_13, chi_23
+        M = np.array([5, 1, 5])
+        chi = [0.5, 1, 0.5]  # chi_12, chi_13, chi_23
     elif name is 'bz65':
-        M = np.array([1,1,64])
-        chi = [0.2,0.3,1] # chi_12, chi_13, chi_23
+        M = np.array([1, 1, 64])
+        chi = [0.2, 0.3, 1]  # chi_12, chi_13, chi_23
     elif name is 'FHPaper':
         chiset = floryhuggins[fhid]
-        solvent,polymer,PCBM = chiset['name'].split("/")
-        M = [N[solvent],N[polymer],N[PCBM]]
-        #M = np.array([1.0,64,5])
+        solvent, polymer, PCBM = chiset['name'].split("/")
+        M = [N[solvent], N[polymer], N[PCBM]]
+        # M = np.array([1.0,64,5])
         chi = np.asarray(chiset['chi'])
-        fname = "_".join([solvent,polymer,PCBM])
+        fname = "_".join([solvent, polymer, PCBM])
     elif name is 'temp':
-        M = np.array([1.0,64.0,1.0])
-        chi = [1.0,0.2,0.3] # chi_12, chi_13, chi_23
+        M = np.array([1.0, 64.0, 1.0])
+        chi = [1.0, 0.2, 0.3]  # chi_12, chi_13, chi_23
     else:
         raise KeyError('No such data exists')
-    CHI = np.array([[0,chi[0],chi[1]],[chi[0],0,chi[2]],[chi[1],chi[2],0]])    
-    
-    info = {'params':r'M:{},$\chi$:{}'.format(M,chi),'fname':fname}
-    
+    CHI = np.array([[0, chi[0], chi[1]], [chi[0], 0, chi[2]], [chi[1], chi[2], 0]])
+
+    info = {'params': r'M:{},$\chi$:{}'.format(M, chi), 'fname': fname}
+
     return M, CHI, info
 
 
-def compute_chemical_potential(phi,m,chi):
-    mu1 = (phi[1]**2)*chi[0] + chi[1]*(phi[2]**2) + \
-    phi[2]*(1-(1/m[2]) + phi[1]*(chi[0]+chi[1]-chi[2])) + np.log(phi[0])
-    
-    mu2 = chi[0]*(phi[1]-1)**2 + chi[1]*phi[2]**2 - phi[2]/m[2] + \
-    phi[2]*((1 + (phi[1]-1))*(chi[0]+chi[1])+chi[2] - phi[1]*chi[2]) + np.log(phi[1])
-    
-    mu3 = 1 - phi[2] + m[2]*(-1 + chi[1] + chi[1]*phi[2]**2) + \
-    m[2]*(phi[2]*(1-2*chi[1]+phi[1]*(chi[0] + chi[1]-chi[2])) + phi[1]*(chi[0]*(phi[1]-1)-chi[1] + chi[2])) + np.log(phi[2])
-    
-    return np.array([mu1,mu2,mu3])
+def compute_chemical_potential(phi, m, chi):
+    mu1 = (phi[1] ** 2) * chi[0] + chi[1] * (phi[2] ** 2) + \
+          phi[2] * (1 - (1 / m[2]) + phi[1] * (chi[0] + chi[1] - chi[2])) + np.log(phi[0])
+
+    mu2 = chi[0] * (phi[1] - 1) ** 2 + chi[1] * phi[2] ** 2 - phi[2] / m[2] + \
+          phi[2] * ((1 + (phi[1] - 1)) * (chi[0] + chi[1]) + chi[2] - phi[1] * chi[2]) + np.log(phi[1])
+
+    mu3 = 1 - phi[2] + m[2] * (-1 + chi[1] + chi[1] * phi[2] ** 2) + \
+          m[2] * (phi[2] * (1 - 2 * chi[1] + phi[1] * (chi[0] + chi[1] - chi[2])) + phi[1] * (
+                chi[0] * (phi[1] - 1) - chi[1] + chi[2])) + np.log(phi[2])
+
+    return np.array([mu1, mu2, mu3])
