4728695ca4844ef93c4e61b00d3dffbbba39f0cf
Kiran Vaddi <kiranvad@cpn-p28-08.cbls.ccr.buffalo.edu>
2021-03-10 16:45:12 -0500

    adding unittests for the first time. Included a fully working test_core.py

--- polyphase/utils.py
+++ polyphase/utils.py
@@ -13,6 +13,18 @@ from scipy.spatial import Delaunay
 
 from scipy.constants import gas_constant
 
+import time
+
+class timer:
+    def __init__(self):
+        self.start = time.time()
+    def end(self):
+        end = time.time()
+        hours, rem = divmod(end-self.start, 3600)
+        minutes, seconds = divmod(rem, 60)
+
+        return "{:0>2} Hr:{:0>2} min:{:05.2f} sec".format(int(hours),int(minutes),seconds)
+
 def _utri2mat(utri, dimension):
     """ convert list of chi values to a matrix form """
     inds = np.triu_indices(dimension,1)
